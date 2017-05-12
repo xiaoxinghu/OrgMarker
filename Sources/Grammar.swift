9e6ae0ada582cc82aa8c88dfcdbf0859cd7177ec
Xiaoxing Hu <dawnstar.hu@gmail.com>
2017-05-12 15:11:41 +1200

    rename: underlined -> underline

--- Sources/Grammar.swift
+++ Sources/Grammar.swift
@@ -18,33 +18,33 @@ fileprivate func surrounded(by mark: String) -> String {
 
 struct Grammar {
   var patterns: [Pattern]
-  
-  
+
+
   mutating func modify(pattern name: String, to become: (Pattern) -> Pattern) {
     guard let index = patterns.index(where: { $0.name == name }) else {
       return
     }
-    
+
     patterns[index] = become(patterns[index])
-    
+
   }
-  
+
   static func main(
     todo: [String] = ["TODO", "DONE"],
     priority: String = "ABC",
     planning: [String] = ["DEADLINE", "SCHEDULED", "CLOSED"]) -> Grammar {
-    
+
     let todoPattern = "(?:(\(todo.joined(separator: "|")))\(space)+)"
     let priorityPattern = "(?:\\[#([\(priority)])\\]\\s+)"
     let planningKeywordPattern = "(\(planning.joined(separator: "|")))"
     let tagsPattern = "(?:\(space)+((?:\\:.+)+\\:)\(space)*)"
-    
+
     return Grammar(patterns: [
       Pattern("blank", match: "^\(space)*\(eol)"),
-      
+
       Pattern("setting", match: "^#\\+([a-zA-Z_]+):\(space)*([^\\n]*)\(eol)",
         captures: [ Capture(1, name: "setting.key"), Capture(2, name: "setting.value") ]),
-      
+
       Pattern("headline", match: "^(\\*+)\(space)\(todoPattern)?\(priorityPattern)?(.*?)\(tagsPattern)?\(eol)",
         captures: [
           Capture(1, name: "headline.stars"),
@@ -53,16 +53,16 @@ struct Grammar {
           Capture(4, name: "headline.text", parseWith: Grammar.inline),
           Capture(5, name: "headline.tags")
         ]),
-      
+
       Pattern("planning", match: "^\(space)*\(planningKeywordPattern):\(space)+(.+?)\(space)*\(eol)",
         captures: [ Capture(1, name: "planning.keyword"), Capture(2, name: "planning.timestamp") ]),
-      
-      
-      
+
+
+
       Pattern("horizontalRule", match: "^\(space)*-{5,}\(eol)"),
-      
+
       Pattern("comment", match: "^\(space)*#\(space)+(.*)\(eol)"),
-      
+
       Pattern("list.item", match: "^(\(space)*)([-+*]|\\d+(?:\\.|\\)))\(space)+(?:(\\[[ X-]\\])\(space)+)?(.*)\(eol)",
         captures: [
           Capture(1, name: "list.item.indent"),
@@ -70,13 +70,13 @@ struct Grammar {
           Capture(3, name: "list.item.checker"),
           Capture(4, name: "list.item.text", parseWith: Grammar.inline),
           ]),
-      
+
       Pattern("footnote", match: "^\\[fn:(\\d+)\\](?:\(space)+(.*))?\(eol)",
         captures: [ Capture(1, name: "footnote.label"), Capture(2, name: "footnote.content") ]),
-      
+
       Pattern("table.separator", match: "^\(space)*\\|-.*\(eol)"),
       Pattern("table.row", match: "^\(space)*\\|(?:[^\\r\\n\\|]*\\|?)+\(eol)"),
-      
+
       Pattern("block", match: "^\(space)*#\\+begin_([a-z]+)(?:\(space)+([^\\n]*))?\(eol)(.*?\(eol))??\(space)*#\\+end_\\1\(eol)",
         options: [.caseInsensitive, .dotMatchesLineSeparators],
         captures: [
@@ -84,22 +84,22 @@ struct Grammar {
           Capture(2, name: "block.params"),
           Capture(3, name: "block.content"),
           ]),
-      
+
       Pattern("drawer", match: "^\(space)*:((?!end|END)[a-zA-Z]+):\(space)*\(eol)(.*?\(eol))??\(space)*:(end|END):\(space)*\(eol)",
         options: [.caseInsensitive, .dotMatchesLineSeparators],
         captures: [
           Capture(1, name: "drawer.name"),
           Capture(2, name: "drawer.content"),
           ]),
-      
+
       Pattern("line", match: "^[^\\n]+\(eol)"),
       ])
   }
-  
+
   static let blockContent = Grammar(patterns: [
     Pattern("block.content", match: ".*")
     ])
-  
+
   static let inline = Grammar(patterns: [
     Pattern("link", match: "\\[\\[([^\\]]*)\\](?:\\[([^\\]]*)\\])?\\]",
             options: [.dotMatchesLineSeparators],
@@ -111,7 +111,7 @@ struct Grammar {
     Pattern("verbatim", match: surrounded(by: "=")),
     Pattern("bold", match: surrounded(by: "\\*")),
     Pattern("italic", match: surrounded(by: "/")),
-    Pattern("underlined", match: surrounded(by: "_")),
+    Pattern("underline", match: surrounded(by: "_")),
     Pattern("strikeThrough", match: surrounded(by: "\\+")),
     ])
 }
